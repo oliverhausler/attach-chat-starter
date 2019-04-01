eb93810054a07a4d1e554228b772d38719b0e3e0
georginio <ggola93@gmail.com>
2019-04-01 17:13:22 +0400

    Toggle pickers on click on screens <= tablet

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
-var defaultEditorColor = "#4D368A";
-var defaultViewerColor = "#01AAEA";
+const shapeList = document.querySelector(".shape__list");
+const pickerValues = document.querySelectorAll(".picker-value");
+
+const defaultEditorColor = "#4D368A";
+const defaultViewerColor = "#9F14C0";
+const colors = [
+  "#F23A2F",
+  "#D31B5B",
+  "#9F14C0",
+  "#4D368A",
+  "#434CC6",
+  "#44BA4F",
+  "#009C8E",
+  "#11ABD8",
+  "#01AAEA",
+  "#0798F0",
+  "#88CB2D",
+  "#CFDB02",
+  "#F6E912",
+  "#FBC705",
+  "#FD9200",
+  "#000002",
+  "#5D7E92",
+  "#9B99A9",
+  "#796B5F",
+  "#F95A10"
+];
 
 new ColorPicker({
   id: "color-editor",
   defaultValue: defaultEditorColor,
-  colors: [
-    "#F23A2F",
-    "#D31B5B",
-    "#9F14C0",
-    "#4D368A",
-    "#434CC6",
-    "#44BA4F",
-    "#009C8E",
-    "#11ABD8",
-    "#01AAEA",
-    "#0798F0",
-    "#88CB2D",
-    "#CFDB02",
-    "#F6E912",
-    "#FBC705",
-    "#FD9200",
-    "#000002",
-    "#5D7E92",
-    "#9B99A9",
-    "#796B5F",
-    "#F95A10"
-  ],
+  colors: colors,
   onChange: function(newColor) {
     updateChatSDK({ editorColor: newColor });
+    updatePicker("editor", newColor);
   }
 });
 
 new ColorPicker({
   id: "color-viewer",
   defaultValue: defaultViewerColor,
-  colors: [
-    "#F23A2F",
-    "#D31B5B",
-    "#9F14C0",
-    "#4D368A",
-    "#434CC6",
-    "#44BA4F",
-    "#009C8E",
-    "#11ABD8",
-    "#01AAEA",
-    "#0798F0",
-    "#88CB2D",
-    "#CFDB02",
-    "#F6E912",
-    "#FBC705",
-    "#FD9200",
-    "#000002",
-    "#5D7E92",
-    "#9B99A9",
-    "#796B5F",
-    "#F95A10"
-  ],
+  colors: colors,
   onChange: function(newColor) {
     updateChatSDK({ viewerColor: newColor });
+    updatePicker("viewer", newColor);
   }
 });
 
-var shapeList = document.querySelector(".shape__list");
+document.addEventListener("click", hideOptions);
+pickerValues.forEach(el => el.addEventListener("click", showOptions));
 shapeList.addEventListener("click", function(e) {
-  updateChatSDK({ shape: e.target.value });
+  const shape = e.target.value;
+  updateChatSDK({ shape });
+  updatePicker("shape", shape);
 });
 
-var updateChatSDK = function({ editorColor, viewerColor, shape }) {
+const updateChatSDK = function({ editorColor, viewerColor, shape }) {
   if (editorColor) {
     attachSdk.setProperty("attach:chat:editor-background-color", editorColor);
   }
@@ -83,21 +71,3 @@ updateChatSDK({
   viewerColor: defaultViewerColor,
   shape: "round"
 });
-
-var currentValues = document.querySelectorAll(".current-value");
-currentValues.forEach(el => el.addEventListener("click", showOptions));
-
-function closest(el, className) {
-  while ((el = el.parentElement) && !el.classList.contains(className));
-  return el;
-}
-
-function _toggleClass(el, className) {
-  el.classList.toggle(className);
-}
-
-function showOptions(e) {
-  const el = e.target;
-  const item = closest(el, "item");
-  _toggleClass(item.querySelector(".pick__list"), "show");
-}
